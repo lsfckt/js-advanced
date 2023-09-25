26f82cdf3d1f04288ee28c3b34749b428af57ac9
Aleksandar Stoyanov <132505560+lsfckt@users.noreply.github.com>
2023-09-25 16:45:54 +0300

    Update 02-greatest-common-divisor.js

--- Syntax, Functions and Statements EXERCISE/02-greatest-common-divisor.js
+++ Syntax, Functions and Statements EXERCISE/02-greatest-common-divisor.js
@@ -1,15 +1,5 @@
 function gdc(first, second) {
-
-    if (second === 0) {
-        return first;
-    } else {
-        return gdc(second, first % second)
-    }
-
+    
+    return second === 0 ? first : gdc(second, first % second);
+    
 }
-let result = gdc(15, 5);
-// 5
-let result2 = gdc(2154, 458);
-// 2
-console.log(result);
-console.log(result2);
