1003588d42ec8ddefc4ad147ed1b0c38b828ef1a
lsfckt <132505560+lsfckt@users.noreply.github.com>
2023-10-11 21:42:31 +0300

    add

--- DOM Introduction EXERCISE/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/08. Generate Report/report.js
+++ DOM Introduction EXERCISE/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/08. Generate Report/report.js
@@ -1,3 +1,23 @@
 function generateReport() {
-    //TODO
+
+    let output = document.querySelector('#output');
+    let checkBoxInputs = document.querySelectorAll('th input');
+    let tableRows = document.querySelectorAll('tbody tr');
+
+    let result = [];
+
+    for (let i = 0; i < tableRows.length; i++) {
+        let report = {};
+
+        for (let j = 0; j < checkBoxInputs.length; j++) {
+
+            if (checkBoxInputs[j].checked) {
+                report[checkBoxInputs[j].name] = tableRows[i].children[j].textContent;
+            }
+        }
+        result.push(report);
+    }
+
+    output.textContent = JSON.stringify(result);
+
 }
